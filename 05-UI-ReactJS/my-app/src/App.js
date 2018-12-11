3797bef4a76e02fc0022d057d6b4339877881e69
Roberto David Marcos <roberto.marcos@globant.com>
2018-12-11 16:49:02 -0300

    trabajando con react

---
+++ 05-UI-ReactJS/my-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './App.css';
+
+class App extends React.Component{
+  constructor(){
+    this.state = {
+        movie = []
+    }
+  }
+
+
+  render(){
+    return(
+      <div id="container">
+       
+      </div>
+    )
+  }
+}
+
+export default App
