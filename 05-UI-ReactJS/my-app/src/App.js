1fd8152133e5414daa0c97a282833c0819a7e36e
Roberto David Marcos <roberto.marcos@globant.com>
2018-12-12 14:33:07 -0300

    agregada funcionalidad: agregar movie

--- 05-UI-ReactJS/my-app/src/App.js
+++ 05-UI-ReactJS/my-app/src/App.js
@@ -1,22 +1,42 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
+import MovieCreator from './MovieCreator.js';
 import './App.css';
 
 class App extends React.Component{
   constructor(){
+    super();
     this.state = {
-        movie = []
+        movies:[{name:'The Terminator',author:'Rober'},{name:'Volver al futuro',author:'Lucho'}]
     }
+    this.addMovie=this.addMovie.bind(this);
+    this.show=this.show.bind(this);
+  }
+
+ 
+  addMovie (movie) {
+    this.setState({
+        movies: [...this.state.movies, movie]
+      })
+
+  }
+
+  show (){
+    for(var i=0;i<this.state.movies.length;i++)
+      console.log(this.state.movies[i].name);
   }
 
 
   render(){
     return(
       <div id="container">
-       
+          <MovieCreator onAddMovie={this.addMovie} onShow={this.show}/>
+          
       </div>
     )
   }
 }
+
+
 
 export default App
---
+++ 05-UI-ReactJS/my-app/src/MovieCreator.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+
+
+class MovieCreator extends React.Component{
+constructor(){
+    super();
+    this.state = {
+        name:'',
+        author:''
+    }
+    this.refName=React.createRef();// mejor aca que en las etiquetas
+    this.refAuthor=React.createRef();
+    this.onSubmit=this.onSubmit.bind(this);
+    this.mostrar=this.mostrar.bind(this);
+}
+
+onSubmit(){
+    const movie = {name:this.refName.current.value, author:this.refAuthor.current.value}
+    
+    this.props.onAddMovie(movie);
+}
+
+mostrar(){
+    this.props.onShow();
+}
+
+
+
+    render(){
+        return(
+            <div>
+                <input placeholder="Ingresa nombre de peli"  ref={this.refName} /> 
+
+                <hr />
+
+                <input placeholder="Ingresa el autor" ref={this.refAuthor}/>
+
+                <hr />
+
+                <button type="submit" onClick={this.onSubmit}> Submit! </button>
+                <button type="submit" onClick={this.mostrar}> Mostrar! </button>
+            </div>
+            
+        )
+    }
+}
+
+
+
+
+export default MovieCreator
+
+ /* render(){
+    return(
+      <div id="container">
+          <Input ref={component => this.a = component} update={this.update.bind(this)} />
+          {this.state.a}
+          <hr />
+          <Input ref={component => this.b = component} update={this.update.bind(this)} />
+          {this.state.b}
+          <Button ref={component => this.input = component} setear={this.setear.bind(this)} />
+          
+      </div>
+    )
+  }
+}
+
+class Input extends React.Component{
+  render(){
+    return(
+      <div><input type="text" ref="input" onChange={this.props.setear}/></div>
+    )
+  }
+}
+
+class Button extends React.Component{
+  render(){
+    return(
+      <button ref="submit" type="submit" onClick={this.props.setear}>Submit!</button>
+    )
+  }
+}*/
+
